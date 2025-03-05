241a3980b173e966741215d84a225c7c75b53a9a
Rachel Skinner <skinner.rachelc@gmail.com>
2025-03-05 18:00:41 +0000

    adding logic for adding task

--- frontend/components/Todo.tsx
+++ frontend/components/Todo.tsx
@@ -15,10 +15,27 @@ const TodoApp: React.FC = () => {
 
     //State to hold an input value with type string
     const [task, setTask] = useState<string>("");
+
+    //add task function
+    const addTodo = () => {
+        if (task.trim() ==="") //trim method will take away leading/trailing whitespace
+            return; //early return bypasses if empty task is trying to be added
+    
+        const newTodo: Todo = {  //Todo type to ensure new todo task follows same structure
+            id: Date.now(), //unique id number generated
+            text: task, //the task being added
+            completed: false, //false when adding new task
+        };
+        setTodos([...todos, newTodo]); //updates state of todo list
+        /* using spread operator because it will append the new task
+        to the current list of to-dos.  */
+        setTask("");  //clears input after submitted so it is empty string
+        };
    
     return (
         <div>
             <h2>To Do App</h2>
+
         </div>
     )
 };
